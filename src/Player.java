3b902b94232530c87069ac60448577f35c48acb9
vlombardi22 <vhl2@live.com>
2018-05-02 18:28:25 -0700

    added documentation

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,11 @@
+/**
+ * This class stores a player object which has a board object and a name.
+ *
+ * CPSC 224-01, Spring 2018
+ * Final Project
+ * @author Vincent Lombardi
+ * @version V1.0 5/3/20
+ */
 
 // package com.lvm.battleship.players;
 
@@ -5,25 +13,44 @@
 
 public class Player {
 
-    private String name;
-    private Board armada;
-    
+    private String name; // player name
+    private Board armada; // player board
+
+    /**
+     * constructor
+     */
     public Player(){
         armada = new Board();
     }
 
+    /**
+     * names get method
+     * @return players name
+     */
     public String getName() {
         return this.name;
     }
-    
+
+    /**
+     * name's set method
+     * @param name string representing new name
+     */
     public void setName(String name){
         this.name = name;
     }
-    
+
+    /**
+     * boards get method
+     * @return players board
+     */
     public Board getArmada(){
         return this.armada;
     }
-    
+
+    /**
+     * boards set method
+     * @param armada new board
+     */
     public void setArmada(Board armada){
         this.armada = armada;
     }
